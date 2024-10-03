f917add25fcebe4d661b74ba6253e8baf9f58379
Zachary Polof <ZacharyPolof@gmail.com>
2024-10-02 22:12:47 -0500

    Info
    Defined info component, created basic structure and styled to look similar to digital-business figma file

--- digital-business/src/components/Info/Info.jsx
+++ digital-business/src/components/Info/Info.jsx
@@ -1,9 +1,14 @@
-
+import './Info.css'; 
+import avatar from '../../assets/zach.jpg';
 
 function Info() {
   return (
     <div className="info">
-      <h1>Info</h1>
+      <img src={avatar} alt="Zachary Polof" className="avatar" />
+      <h1>Zachary Polof</h1>
+      <h2>Frontend Developer</h2>
+      <a href="mailto:ZacharyPolof@gmail.com" className="btn">Email</a>
+      <a href="https://www.linkedin.com/in/zacharypolof/" className="btn">LinkedIn</a>
     </div>
   );
 }
--- digital-business/src/main.jsx
+++ digital-business/src/main.jsx
@@ -10,4 +10,3 @@ createRoot(document.getElementById('root')).render(
 )
 
 
-// create separate files for each component Personal info, about me, interests, social icons, and footer
