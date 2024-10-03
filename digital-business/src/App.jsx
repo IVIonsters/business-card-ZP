38e772223fcdd8e6aac50ee7bc03f9c1b8e5b674
Zachary Polof <ZacharyPolof@gmail.com>
2024-10-02 22:16:27 -0500

    Restrucute
    Added App.jsx and .css back into project and re-rendered
    
    styled card in main App.jsx file

---
+++ digital-business/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Info from './components/Info/Info';
+import './App.css';
+
+function App() {
+  return (
+    <div className="card">
+      <Info />
+      {/* <About />
+      <Interests />
+      <Footer /> */}
+    </div>
+  );
+}
+
+export default App;
--- digital-business/src/main.jsx
+++ digital-business/src/main.jsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Info from './components/Info/Info.jsx'
+import App from './App'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Info />
+    <App />
   </StrictMode>,
 )
 
