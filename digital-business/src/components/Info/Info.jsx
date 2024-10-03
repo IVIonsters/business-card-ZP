d6f69fe24913550ee87800a5fb55db7b096f59e9
Zachary Polof <ZacharyPolof@gmail.com>
2024-10-02 22:39:09 -0500

    Info Component
    Restyled image to fit business card size instead of previous view of avatar

--- digital-business/src/components/Info/Info.jsx
+++ digital-business/src/components/Info/Info.jsx
@@ -4,7 +4,7 @@ import avatar from '../../assets/zach.jpg';
 function Info() {
   return (
     <div className="info">
-      <img src={avatar} alt="Zachary Polof" className="avatar" />
+      <img src={avatar} alt="Zachary Polof" className="header-image" />
       <h1>Zachary Polof</h1>
       <h2>Frontend Developer</h2>
       <a href="mailto:ZacharyPolof@gmail.com" className="btn">Email</a>
