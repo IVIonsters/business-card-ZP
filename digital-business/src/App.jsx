85af3f783febb67e13724b24031a056bb16b5666
Zachary Polof <ZacharyPolof@gmail.com>
2024-10-02 22:19:57 -0500

    About Section
    added About section
    
    created component and styled

--- digital-business/src/App.jsx
+++ digital-business/src/App.jsx
@@ -1,14 +1,14 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import Info from './components/Info/Info';
+import About from './components/About/About';
 import './App.css';
 
 function App() {
   return (
     <div className="card">
       <Info />
-      {/* <About />
-      <Interests />
-      <Footer /> */}
+      <About />
     </div>
   );
 }
---
+++ digital-business/src/components/About/About.jsx
@@ -0,0 +1,17 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import './About.css';
+
+function About() {
+  return (
+    <div className="about">
+      <h3>About</h3>
+      <p>
+        I am a full stack developer with a strong focus on building dynamic, responsive applications using React and the MERN stack. 
+        I specialize in creating scalable and maintainable solutions that enhance user experiences.
+      </p>
+    </div>
+  );
+}
+
+export default About;
